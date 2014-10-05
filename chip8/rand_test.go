71a6e8be46b812a86ada89b3cd5f3e4bd6d6d37e
Peter McAtominey <mcatominey08@gmail.com>
2014-10-05 02:38:53 +0100

    Added rand_test.go

---
+++ chip8/rand_test.go
@@ -0,0 +1,34 @@
+package chip8
+
+import (
+	"testing"
+)
+
+// This test is in no way comprehensive or well designed it only
+// serves to ensure that Rand doesn't return the same result constantly
+func TestRand(t *testing.T) {
+	iterations := 100
+	r := Rand{}
+
+	last := r.Byte()
+	for i := 0; i < iterations; i++ {
+		if r.Byte() != last {
+			return
+		}
+	}
+
+	t.Errorf("random byte generator returned same value %i times", iterations)
+}
+
+// Same as above applies
+func TestMockRand(t *testing.T) {
+	iterations := 100
+	r := MockRand{}
+
+	last := r.Byte()
+	for i := 0; i < iterations; i++ {
+		if r.Byte() != last {
+			t.Error("mock random byte generator returned differing value")
+		}
+	}
+}
